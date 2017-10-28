b997e5efe9f2217dbce1e60928624c439a9764b3
Hack Reactor Student <student@hackreactor.com>
2017-10-27 17:34:45 -0700

    correct templateUrl issue

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -6,5 +6,16 @@ angular.module('video-player')
     
     controller: function() {},
     
-    templateUrl:'/src/templates/videoListEntry.html'
+    // templateUrl: '/src/templates/videoListEntry.html'
+    template: 
+    `<li class="video-list-entry media">
+      <div class="media-left">
+        <img class="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" />
+      </div>
+      <div class="media-body">
+        <div class="video-list-entry-title">Video Title: {{$ctrl.video.snippet.title}} </div>
+        <div class="video-list-entry-detail">Video Description: {{$ctrl.video.snippet.description}}</div>
+      </div>
+    </li>
+    `
   });
