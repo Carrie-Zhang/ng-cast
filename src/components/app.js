45a44bda72a8c939f3ecdaf6124d90b7960da7ab
Hack Reactor Student <student@hackreactor.com>
2017-10-28 16:56:40 -0700

    finish search

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,24 @@
 angular.module('video-player')
   .component('app', {
+    templateUrl: '/src/templates/app.html',
     controller(youTube) {
+      this.searchVideo = '',
       this.videos = window.exampleVideoData,
       this.currentVideo = window.exampleVideoData[0],
       this.selectVideo = (video) => {
         console.log('in selectVideo function');
         this.currentVideo = video;
       },
-      this.searchResults = () => {};
-      youTube.search(results => {
-        this.selectVideo(results.data.items[0]);
-        // console.log('hellp', results.data.items);
-      });
-    },
-    
-    templateUrl: '/src/templates/app.html'
+      
+      this.searchResults = (query) => {
+        youTube.search(query, results => {
+          this.videos = results.data.items;
+          this.currentVideo = this.videos[0];
+          //this.selectVideo(results.data.items));
+        });
+      };
+      //this.searchResults('cats');
+    }   
   });
   
   
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,10 +1,15 @@
 angular.module('video-player')
   .service('youTube', function($http) {
-    this.search = function(callback) {
+    this.search = function(query, callback) {
       $http({
         method: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
-        params: {'maxResults': 5, 'part': 'snippet', 'q': 'corgi', 'type': 'video', 'key': window.YOUTUBE_API_KEY, 'videoEmbeddable': true}  
+        params: {'maxResults': 5,
+          'part': 'snippet',
+          'q': query,
+          'type': 'video',
+          'key': window.YOUTUBE_API_KEY,
+          'videoEmbeddable': true}  
       }).then(
         function successCallback(response) {
           callback(response);
